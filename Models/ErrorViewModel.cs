b83c35612d8faed607a754f211d5e1e00bae7679
khanhn2 <khanhn@fpt.edu.vn>
2024-10-20 23:04:52 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NutriGuide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
