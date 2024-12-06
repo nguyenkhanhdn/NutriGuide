c6618319adbae3fd1047e40eefa22898fdfa4eae
khanhn2 <khanhn@fpt.edu.vn>
2024-12-06 21:13:26 +0700

    aaa

---
+++ Controllers/PredictResult.cs
@@ -0,0 +1,9 @@
+﻿namespace NutriGuide.Controllers
+{
+    public class PredictResult
+    {
+        public string? Label { get; set; }
+        public float Score { get; set; }
+            
+    }
+}
